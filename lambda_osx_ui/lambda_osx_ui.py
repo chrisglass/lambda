e9a4fdf4c5e69589ef286932d6c2f71b67361a76
Olivier Amblet <olivier@miettes.ch>
2008-09-18 18:34:27 +0200

    First commit

---
+++ lambda_osx_ui/lambda_osx_ui.py
@@ -0,0 +1,18 @@
+#
+#  lambda_osx_ui.py
+#  lambda_osx_ui
+#
+
+from PyObjCTools import NibClassBuilder, AppHelper
+from Foundation import NSBundle
+
+info = NSBundle.mainBundle().infoDictionary()[u'PyObjCXcode']
+
+for nibFile in info[u'NIBFiles']:
+    NibClassBuilder.extractClasses(nibFile)
+
+for pythonModule in info[u'Modules']:
+    __import__(pythonModule)
+
+if __name__ == '__main__':
+    AppHelper.runEventLoop()
---
+++ lambda_osx_ui/lambda_osx_uiAppDelegate.py
@@ -0,0 +1,12 @@
+#
+#  lambda_osx_uiAppDelegate.py
+#  lambda_osx_ui
+#
+
+from Foundation import *
+from AppKit import *
+
+from PyObjCTools import NibClassBuilder
+
+class lambda_osx_uiAppDelegate(NibClassBuilder.AutoBaseClass):
+    pass
